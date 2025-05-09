d781f6ad957b9826bd622977ef4a05f2d17a1add
kerkfeld <s6nikerk@uni-bonn.de>
2025-05-09 14:13:31 +0200

    fixed wrong key

--- src/Adversarial/Dataset.py
+++ src/Adversarial/Dataset.py
@@ -29,7 +29,7 @@ generate_transformations = Compose([
                allow_missing_keys=True),
     AddMissingd(keys=['t1w', 'FLAIR', 'target'], key_add='target', ref='FLAIR'),
     ResampleToMatchd(keys=['target', 't1w', 'FLAIR'], key_dst='target'),
-    CenterSpatialCropd(keys=['t1w', 'FLAIR', 'change'], roi_size=(160, 256, 256)),
+    CenterSpatialCropd(keys=['t1w', 'FLAIR', 'target'], roi_size=(160, 256, 256)),
     NormalizeIntensityd(keys=['t1w', 'FLAIR']),
     ConcatItemsd(keys=['t1w', 'FLAIR'], name='tensor', dim=0),
     ToTensord(keys=['tensor', 'target']),
