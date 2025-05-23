ce66eaca49f374f7c24df9bda2476ac4b4584e79
NKerkfeld <s6nikerk@uni-bonn.de>
2025-05-23 13:10:51 +0200

    fixed missing target

--- src/generate.py
+++ src/generate.py
@@ -8,6 +8,7 @@ from src.Framework.utils import get_network, load_data
 
 def main(steps: int):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
 
     model = get_network(configuration='2d', fold=0)
     generator = ElasticDeformation2D(model, (256, 256), (16, 16))
@@ -15,13 +16,19 @@ def main(steps: int):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    image = load_data('data/Dataset101_fcd/sub-00001', device=device, slice=32)
+    item = load_data('data/Dataset101_fcd/sub-00001', device=device, slice=32)
+    image = item['tensor'].to(device)
+    target = item['target'].to(device)
 
-    for i in trange(steps):
+    print("starting process...")
+
+    bar = trange(steps, desc='generating...')
+    for _ in bar:
         optimizer.zero_grad()
-        loss = generator(image)
+        loss = generator(image, target)
         loss.backward()
         optimizer.step()
+        bar.set_description(f"loss: {loss.detach().cpu().item()}")
 
     new_image = generator.image(image)
 
