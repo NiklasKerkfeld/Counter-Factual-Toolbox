335133a3cd2ba47041a89375fe7f0d2cace7d8ac
kerkfeld <s6nikerk@uni-bonn.de>
2025-06-03 16:16:01 +0200

    ComposeGenerator weights

--- src/Architecture/ComposeGenerator.py
+++ src/Architecture/ComposeGenerator.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import torch
 from torch import nn
@@ -8,15 +8,21 @@ from src.Architecture.Generator import Generator
 
 
 class ComposeGenerator(Generator):
-    def __init__(self, model: nn.Module, loss=CrossEntropyLoss(), alpha: float = 1.0, composion: List[Generator] = []):
+    def __init__(self, model: nn.Module, loss=CrossEntropyLoss(), alpha: float = 1.0,
+                 composition: List[Generator] = [],
+                 weights: Optional[List[float]] = None):
         super(ComposeGenerator, self).__init__( model, loss, alpha)
 
-        self.composion = composion
+        self.composition = composition
+        self.weights = weights if weights is not None else [1.0] * len(composition)
+
+        if len(self.composition) != len(self.weights):
+            raise ValueError("The number of weights and composition must be the same")
 
     def adapt(self, image: torch.Tensor):
         overall_cost = torch.tensor([0.0], device=image.device)
-        for g in self.composion:
+        for g, weight in zip(self.composition, self.weights):
             image, cost = g.adapt(image)
-            overall_cost += cost
+            overall_cost += weight * cost
 
         return image, overall_cost
