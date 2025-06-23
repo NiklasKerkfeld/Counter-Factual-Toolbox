96e0d792d0a41e1bf9154f92a2114ec44f78dcff
kerkfeld <s6nikerk@uni-bonn.de>
2025-06-23 13:53:06 +0200

    fixed model loading error

--- src/Adversarial/Trainer.py
+++ src/Adversarial/Trainer.py
@@ -65,7 +65,7 @@ class Trainer:
                        prediction=pred[0, 1])
 
     def train_adversarial(self):
-        self.generator.load_adversarial(f"models/{self.name}_adversarial.pth")
+        self.generator.load_adversarial(f"{self.name}_adversarial")
         optimizer = torch.optim.Adam(self.generator.adversarial.parameters(), lr=1e-3)
         min_loss = float('inf')
 
